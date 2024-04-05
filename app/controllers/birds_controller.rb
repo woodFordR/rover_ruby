477638219bf638d4b5a10ea42b0f553c0b22f813
woodFordR <adam@kaewell.com>
2024-04-04 23:26:36 -0400

    kamal changes, bird health, routes

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,5 @@
+class BirdsController < ApplicationController
+  def show
+    'word'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
       resources :projects
     end
   end
+
+  get "bird", to: "bird#show"
 end
