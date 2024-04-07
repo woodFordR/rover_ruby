d02fd679bfc7ec02a225036cac1ff19a36c1f5d7
woodFordR <adam@kaewell.com>
2024-04-06 20:45:07 -0400

    adding image and starting seeds to backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,34 @@
 logo_options = ['nomad', 'packer', 'vagrant', 'terraform', 'waypoint']
 
 [
+  # {
+  #   title: "emberman",
+  #   description: "A static ruby site built with middleman and emberjs.",
+  #   logo: "packer",
+  #   owner: "woodFordR"
+  # },
+  # {
+  #   title: "rr-blog",
+  #   description: "A blogging application built with ruby on rails and reactjs.",
+  #   logo: "waypoint",
+  #   owner: "woodFordR"
+  # },
+  # {
+  #   title: "superRentals",
+  #   description: "An emberjs application.",
+  #   logo: "vault",
+  #   owner: "woodFordR"
+  # },
   {
-    title: "emberman",
-    description: "A static ruby site built with middleman and emberjs.",
-    logo: "packer",
-    owner: "woodFordR"
-  },
-  {
-    title: "rr-blog",
-    description: "A blogging application built with ruby on rails and reactjs.",
-    logo: "waypoint",
-    owner: "woodFordR"
-  },
-  {
-    title: "superRentals",
-    description: "An emberjs application.",
-    logo: "vault",
-    owner: "woodFordR"
+    title: "Woodford's Den",
+    sub_title: "My blog built with jekyll static site generator.",
+    description: "Woodford's Den is the name of my blog. I'm in the process of adding more 
+      posts. I tweaked the js on the frontend to support the lottie library. There are animated 
+      coffee cups and puppy paws when you hover over items. Cheers." ,
+    logo: "nomad",
+    owner: "woodFordR",
+    deploy_url: "http://blog.kaewell.com/",
+    code_image: "woodfords_den.png",
   }
 ].each do |project|
   Project.create(project)
