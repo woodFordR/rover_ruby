c79ca058217be1d23f4ad886af71441b9f5bf12d
woodFordR <adam@kaewell.com>
2025-08-20 16:16:10 -0400

    fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,7 @@
 
 [
   {
-    code_snippet: "def grab_links(uri)\n" +
-      "\timage_urls = URI.open(uri) do |f|\n" +
-      "\t\tf.read.scan(/.*?<link.*?as=\"image\" href=(\".*?\").*?/m)\n" +
-      "\tend\n" +
-      "\timage_urls.map { |f| /.*(https.*\.png).*/.match(f[0])[1] }\n" +
-      "end\n",
+    code_snippet: "def grab_links(uri)\n\timage_urls = URI.open(uri) do |f|\n\t\tf.read.scan(/.*?<link.*?as=\"image\" href=(\".*?\").*?/m)\n\tend\n\timage_urls.map { |f| /.*(https.*\.png).*/.match(f[0])[1] }\nend\n",
     code_description: "In this application, ",
     code_link: "https://github.com/woodFordR/emberman/blob/main/extensions/image_tracker.rb",
     deploy_link: "",
@@ -26,13 +21,7 @@
     language: "ruby",
   },
   {
-    code_snippet: "async function fetchAllPosts() {\n" +
-      "\tconst response = await fetch(\`${API_URL}\`);\n" +
-      "\tif (!response.ok) {\n" +
-      "\t\tthrow new Error(response.statusText);\n" +
-      "\t}\n" +
-      "\treturn response.json();\n" +
-      "}\n",
+    code_snippet: "async function fetchAllPosts() {\n\tconst response = await fetch(\`${API_URL}\`);\n\tif (!response.ok) {\n\t\tthrow new Error(response.statusText);\n\t}\n\treturn response.json();\n}\n",
     code_description: "In this application, ",
     code_link: "https://github.com/woodFordR/rr-blog/blob/main/frontend/src/services/postService.js",
     deploy_link: "",
@@ -43,16 +32,7 @@
     language: "javascript",
   },
   {
-    code_snippet: "import Component from '@glimmer/component';\n\n" +
-      "export default class RentalsFilterComponent extends Component {\n" +
-      "\tget results() {\n" +
-      "\t\tlet { rentals, query } = this.args;\n" +
-      "\t\tif (query) {\n" +
-      "\t\t\trentals = rentals.filter((rental) => rental.title.includes(query));\n" +
-      "\t\t}\n" +
-      "\treturn rentals;\n" +
-      "\t}\n" +
-      "}\n",
+    code_snippet: "import Component from '@glimmer/component';\n\nexport default class RentalsFilterComponent extends Component {\n\tget results() {\n\t\tlet { rentals, query } = this.args;\n\t\tif (query) {\n\t\t\trentals = rentals.filter((rental) => rental.title.includes(query));\n\t\t}\n\treturn rentals;\n\t}\n}\n",
     code_description: "In this application, ",
     code_link: "https://github.com/woodFordR/superRentals/blob/main/app/components/rentals/filter.js",
     deploy_link: "",
@@ -63,17 +43,9 @@
     language: "javascript",
   },
   {
-    code_description: "In this blogging application, I have a snippet " +
-      "of code written in JS displaying lottiefiles. Lottiefiles are animations " +
-      "used for applications. This is a simple hover script for the coffee cup.",
+    code_description: "In this blogging application, I have a snippet of code written in JS displaying lottiefiles. Lottiefiles are animations used for applications. This is a simple hover script for the coffee cup.",
     code_link: "https://github.com/woodFordR/blog/blob/main/_layouts/home.html#L49C1-L60C10",
-    code_snippet: "var coffeeLottie = lottie.loadAnimation({\n" +
-      "\tcontainer:coffeeSvg,\n" +
-      "\trenderer:'svg',\n" +
-      "\tloop:true,\n" +
-      "\tautoplay:true,\n" +
-      "\tpath:'lottie/coffee.json'\n" +
-      "});\n",
+    code_snippet: "var coffeeLottie = lottie.loadAnimation({\n\tcontainer:coffeeSvg,\n\trenderer:'svg',\n\tloop:true,\n\tautoplay:true,\n\tpath:'lottie/coffee.json'\n});\n",
     deploy_link: "https://blog.kaewell.com",
     description: "A blog built with ruby and jekyll.rb",
     logo: "terraform",
@@ -82,29 +54,8 @@
     language: "javascript",
   },
   {
-    code_snippet:
-      "module Cookbook\n" +
-      "\tmodule Views\n" +
-      "\t\tmodule Views\n" +
-      "\t\t\tmodule Home\n" +
-      "\t\t\t\tclass Show < Cookbook::View\n" +
-      "\t\t\t\t\texpose :recipes do\n" +
-      "\t\t\t\t\t\t[\n" +
-      "\t\t\t\t\t\t\t{\n" +
-      "\t\t\t\t\t\t\t\tname: \"Cinnamon Rolls\",\n" +
-      "\t\t\t\t\t\t\t\tdescription: \"Big, fluffy, soft and absolutely delicious...\"\n" +
-      "\t\t\t\t\t\t\t}\n" +
-      "\t\t\t\t\t\t]\n" +
-      "\t\t\t\t\tend\n" +
-      "\t\t\t\tend\n" +
-      "\t\t\tend\n" +
-      "\t\tend\n" +
-      "\tend\n" +
-      "end\n",
-    code_description: "In hanami all the 'App' code lives in the same " +
-      "namespace and shares the same name. It is very quick to mock " +
-      "data for a homepage view. This is the view class " +
-      "for the root page.",
+    code_snippet: "class Show < Cookbook::View\n\texpose :recipes do\n\t\t[\n\t\t\t\t{\n\t\t\t\t\t\tname: \"Cinnamon Rolls\",\n\t\t\t\t\t\tdescription: \"Big, fluffy, soft and absolutely delicious...\"\n\t\t\t\t}\n\t\t]\nend\n", 
+    code_description: "In hanami all the 'App' code lives in the same namespace and shares the same name. It is very quick to mock data for a homepage view. This is the view class for the root page.",
     code_link: "https://github.com/woodFordR/cookbook/blob/main/app/views/home/show.rb",
     deploy_link: "",
     description: "A refreshing hanami app built with ruby.",
@@ -126,26 +77,35 @@
   })
 end
 
-# [
-#   {
-#     first_name: "Jimmy",
-#     last_name: "Jokes",
-#     username: "knockKnock",
-#     email: "adam@woodford.life",
-#   },
-#   {
-#     first_name: "Tony",
-#     last_name: "Pepperoni",
-#     username: "hitOrMiss",
-#     email: "test@woodford.life",
-#   },
-#   {
-#     first_name: "Donny",
-#     last_name: "Dimes",
-#     username: "heyyyyyyyAllRight",
-#     email: "dev@woodford.life",
-#   },
-# ].each do |user|
-#   User.create(user)
-# end
+[
+  {
+    first_name: "Jimmy",
+    last_name: "Jokes",
+    username: "knockKnock",
+    email: "adam@woodford.life",
+    track_visits: 3141,
+  },
+  {
+    first_name: "Tony",
+    last_name: "Pepperoni",
+    username: "hitOrMiss",
+    email: "test@woodford.life",
+    track_visits: 5598,
+  },
+  {
+    first_name: "Donny",
+    last_name: "Dimes",
+    username: "heyyyyyyyAllRight",
+    email: "dev@woodford.life",
+    track_visits: 892376,
+  },
+].each do |user|
+  User.create({
+    :first_name => project[:first_name],
+    :last_name => project[:last_name],
+    :username => project[:username],
+    :email => project[:email],
+    :track_visits => project[:track_visits],
+  })
+end
 
