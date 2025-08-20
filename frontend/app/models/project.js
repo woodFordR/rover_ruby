fd846da7d5237ca579395b855a799d2a7303cf19
woodFordR <adam@kaewell.com>
2025-08-20 15:17:43 -0400

    removed extra comments, unused css styling

--- frontend/app/models/project.js
+++ frontend/app/models/project.js
@@ -2,11 +2,11 @@ import Model, { attr } from '@ember-data/model';
 
 export default class Project extends Model {
   @attr('string') avatarPath;
-  @attr('text') codeDescription;
+  @attr('string') codeDescription;
   @attr('string') codeLink;
-  @attr('text') codeSnippet;
+  @attr('string') codeSnippet;
   @attr('string') deployLink;
-  @attr('text') description;
+  @attr('string') description;
   @attr('string') logo;
   @attr('string') owner;
   @attr('string') title;
