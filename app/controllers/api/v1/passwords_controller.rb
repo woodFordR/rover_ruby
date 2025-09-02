bbf5f0fb484c1a07ac0f5d13195ae418406b91ad
woodFordR <adam@kaewell.com>
2025-09-01 21:32:15 -0400

    finished adding passwords controller from clearance with email, flash, redirects commented because we running an api

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -37,12 +37,80 @@ class Api::V1::PasswordsController < Clearance::PasswordsController
       # redirect_to url_after_update, status: :see_other
       session[:password_reset_token] = nil
     else
-      flash_failure_after_update
+      # flash_failure_after_update
       # render template: "passwords/edit", status: :unprocessable_entity
     end
   end
 
   private
 
+  def deliver_email
+    # ::ClearanceMailer.change_password(user).deliver_now
+  end
+
+  def password_from_password_reset_params
+    params.dig(:password_reset, :password)
+  end
+
+  def find_user_by_id_and_confirmation_token
+    user_param = Clearance.configuration.user_id_parameter
+    token = params[:token] || session[:password_reset_token]
+
+    Clearance.configuration.user_model.
+      find_by(id: params[user_param], confirmation_token: token.to_s)
+  end
+
+  def email_from_password_params
+    params.dig(:password, :email, :username)
+  end
+
+  def find_user_for_create
+    Clearance.configuration.user_model.
+      find_by_normalized_email(email_from_password_params)
+  end
+
+  def find_user_for_edit
+    find_user_by_id_and_confirmation_token
+  end
+
+  def find_user_for_update
+    find_user_by_id_and_confirmation_token
+  end
+
+  def ensure_email_present
+    if email_from_password_params.blank?
+      # flash_failure_when_missing_email
+      # render template: "passwords/new", status: :unprocessable_entity
+    end
+  end
+
+  def ensure_existing_user
+    unless find_user_by_id_and_confirmation_token
+      # flash_failure_when_forbidden
+      # render template: "passwords/new", status: :unprocessable_entity
+    end
+  end
+
+  def flash_failure_when_forbidden
+    # flash.now[:alert] = translate(:forbidden,
+    #   scope: [:clearance, :controllers, :passwords],
+    #   default: t("flashes.flash_failure_when_forbidden"))
+  end
+
+  def flash_failure_after_update
+    # flash.now[:alert] = translate(:blank_password,
+    #   scope: [:clearance, :controllers, :passwords],
+    #   default: t("flashes.failure_after_update"))
+  end
+
+  def flash_failure_when_missing_email
+    # flash.now[:alert] = translate(:missing_email,
+    #   scope: [:clearance, :controllers, :passwords],
+    #   default: t("flashes.flash_failure_when_missing_email"))
+  end
+
+  def url_after_update
+    # Clearance.configuration.redirect_url
+  end
 end
 
