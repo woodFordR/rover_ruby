d32b15e83a2bd291a2c50a7803cf765218b8d4c7
woodFordR <adam@kaewell.com>
2025-09-07 16:07:45 -0400

    adding resume and files to user and photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :file do |f|
+    f.variant :medium, resize_to_limit: [192, 108], preprocessed: true
+    f.variant :large, resize_to_limit: [768, 432], preprocessed: true
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  has_many :photos
   has_many :projects
+  has_one_attached :resume
 end
 
