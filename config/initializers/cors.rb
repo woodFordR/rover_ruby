db8e3d0d0e75c449e5a1f36f5c7052196d2f3a5e
woodFordR <adam@kaewell.com>
2025-09-08 18:03:57 -0400

    adding production creds, fixing up production deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "#{Rails.application.credentials.dig(:cors, :origins)}" 
 
     resource "*",
       headers: :any,
